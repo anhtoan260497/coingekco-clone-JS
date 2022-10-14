3e350a5360475be1512607ee8beb4fb15569264b
anhtoan260497 <anhtoan260497@gmail.com>
2022-10-15 00:13:13 +0700

    add header top menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
-import 'antd/dist/antd.min.css'
+import React from "react";
+import "./App.css";
+import "antd/dist/antd.min.css";
+import MenuTopPage from "components/MenuTopPage";
+import { useSelector } from "react-redux";
+import clsx from "clsx";
 
 function App() {
+  const isDay = useSelector(state => state.darkModeSlice.isDay)
   return (
-    <div className='App'>
+    <div className={clsx('App',!isDay && 'dark')}>
+      <div className="App-container">
+        <MenuTopPage />
+      </div>
     </div>
   );
 }
--- src/features/darkModeSlice.js
+++ src/features/darkModeSlice.js
@@ -3,8 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const calculateIsDay = () => {
     const date =  new Date()
     const hours =  date.getHours()
-    console.log(hours)
-    if(hours >= 6 || hours < 18) return true
+    if(hours >= 6 && hours < 18) return true
     return false
 } //if day return true, else false
 
